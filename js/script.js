36a9e36f2567fb197c44fe34337764aafe057eb6
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-11-23 14:39:05 +0300

    Show data to the page and add styles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-// fetch("https://www.omdbapi.com/?apiKey=f88ba9a5&t=merlin")
-//   .then((data) => data.json())
-//   .then((api) => console.log(api))
-//   .catch((err) => console.log(err));
-
+// const url =
+//   "https://moviesdatabase.p.rapidapi.com/titles/search/keyword/merlin";
 // const options = {
 //   method: "GET",
-//   url: "https://moviesdatabase.p.rapidapi.com/titles/series/%7BseriesId%7D",
 //   headers: {
 //     "X-RapidAPI-Key": "b1350477bamsh9fd0f126c02870ep11df03jsn1fd0c3303aaf",
 //     "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
@@ -13,28 +9,52 @@
 // };
 
 // try {
-//   const response = await axios.request(options);
-//   console.log(response.data);
+//   const response = await fetch(url, options);
+//   const result = await response.json();
+//   console.log(result);
 // } catch (error) {
 //   console.error(error);
 // }
 
-const url =
-  "https://moviesdatabase.p.rapidapi.com/titles/search/keyword/merlin";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "b1350477bamsh9fd0f126c02870ep11df03jsn1fd0c3303aaf",
-    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-  },
-};
+// %7Bkeyword%7D
 
-try {
-  const response = await fetch(url, options);
-  const result = await response.json();
-  console.log(result);
-} catch (error) {
-  console.error(error);
+const apiurl =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
+
+// initially get fav movies
+getMovies(apiurl);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
+  const respData = await resp.json();
+
+  console.log(respData);
+
+  showMovies(respData.results);
 }
 
-// %7Bkeyword%7D
+function showMovies(movies) {
+  movies.forEach((movie) => {
+    const { title, backdrop_path, vote_average, poster_path } = movie;
+
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+        <img src="${IMGPATH + poster_path}" alt="${title}" />
+
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span>${vote_average}</span>
+        </div>
+    `;
+
+    div.classList.add("movie");
+
+    main.appendChild(div);
+  });
+}
